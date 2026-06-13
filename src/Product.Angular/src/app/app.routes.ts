23c03fec1f2a436ad8c176b006f6dfef755cca41
Bruno Dias <BrunoCesarODias@gmail.com>
2026-06-13 12:46:03 -0300

    Add gitignore

--- src/Product.Angular/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProductList } from './pages/product-list/product-list';
-
-export const routes: Routes = [
-    {
-        component: ProductList,
-        path: ''
-    }
-];
